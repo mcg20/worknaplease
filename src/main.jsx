ce511563e012cb82ff84fa56e4eb1e20bac7d39f
Princess Mcglence Gamotia <princessmcglence.gamotia@g.msuiit.edu.ph>
2024-05-26 21:36:10 +0800

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/Router.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react"
 import AuthProvider from "./contexts/AuthProvider.jsx";
 
 // TanStack Query
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -79,7 +79,7 @@ const router = createBrowserRouter([
         {
           path: 'update-menu/:id',
           element: <UpdateMenu/>,
-          loader: ({ params }) => fetch(`http://localhost:5000/menu/${params.id}`)
+          loader: ({ params }) => fetch(`https://final-food-casestudy2.onrender.com//menu/${params.id}`)
         },
         {
           path: 'bookings',
